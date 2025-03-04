308ba02d06ca6150e0cfab460a4f1a45a486cb4f
haroonsiddiqqureshi <haroonsiddiq.q@kkumail.com>
2025-03-05 00:12:45 +0700

    commit

--- api/index.py
+++ api/index.py
@@ -55,15 +55,16 @@ def predict():
 @app.route("/predict/line", methods=["POST"])
 def predict_line():
     try:
-        cap_surface = (int(request.form["cap_surface"]),)
-        odor = (int(request.form["odor"]),)
-        gill_spacing = (int(request.form["gill_spacing"]),)
-        gill_size = (int(request.form["gill_size"]),)
-        stalk_root = (int(request.form["stalk_root"]),)
-        stalk_surface_below_ring = (int(request.form["stalk_surface_below_ring"]),)
-        spore_print_color = (int(request.form["spore_print_color"]),)
-        population = (int(request.form["population"]),)
-        habitat = (int(request.form["habitat"]),)
+        data = request.get_json() 
+        cap_surface = data["cap-surface"]
+        odor = data["odor"]
+        gill_spacing = data["gill-spacing"]
+        gill_size = data["gill-size"]
+        stalk_root = data["stalk-root"]
+        stalk_surface_below_ring = data["stalk-surface-below-ring"]
+        spore_print_color = data["spore-print-color"]
+        population = data["population"]
+        habitat = data["habitat"]
 
         input_data = (
             cap_surface,
@@ -76,7 +77,7 @@ def predict_line():
             population,
             habitat,
         )
-        input_array = np.array([list(input_data.values())], dtype=np.float32)
+        input_array = np.array([input_data], dtype=np.float32)
 
         inputs = {ort_session.get_inputs()[0].name: input_array.reshape(1, -1)}
         prediction = ort_session.run(None, inputs)
@@ -100,6 +101,7 @@ def predict_line():
         )
 
     except Exception as e:
+        print(e)
         return (
             jsonify({"message": "An error occurred", "error": str(e), "status": 500}),
             500,
